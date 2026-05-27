697998a6e6f14a33f1cc819a3407ff48ac7c8b09
Shema-USK <illya.shema4527482@gmail.com>
2026-05-28 01:49:47 +0300

    Add Auditorium creation controller and view
    Implemented AuditoriumsController with create actions and form validation. Added a strongly-typed Create.cshtml view for auditorium entry. Included an unused Razor Page model for Create, though MVC is used.

---
+++ DB_ex_proj/Controllers/AuditoriumController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DB_ex_proj.Models;
+
+namespace DB_ex_proj.Controllers
+{
+    public class AuditoriumsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuditoriumsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Auditorium auditorium)
+        {
+            ModelState.Remove("Group");
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(auditorium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(auditorium);
+        }
+    }
+}
---
+++ DB_ex_proj/Views/Auditorium/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DB_ex_proj.Views.Auditorium
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
