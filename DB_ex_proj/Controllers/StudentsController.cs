b172ca199bb181c21039650bf17fd241ca0f5386
Shema-USK <illya.shema4527482@gmail.com>
2026-05-27 19:24:05 +0300

    Add student creation form and controller actions
    Implemented StudentsController with actions to render and process the student creation form. Added Create.cshtml Razor view with Bootstrap styling and validation for student registration. Included group selection from the database. Also added a minimal Create.cshtml.cs PageModel, though not used in the current MVC flow.

---
+++ DB_ex_proj/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DB_ex_proj.Models;
+
+public class StudentsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public StudentsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Groups = new SelectList(_context.Groups, "Id", "Name");
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Student student)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Groups = new SelectList(_context.Groups, "Id", "Name", student.GroupId);
+        return View(student);
+    }
+}
---
+++ DB_ex_proj/Views/Students/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DB_ex_proj.Views.Students
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
