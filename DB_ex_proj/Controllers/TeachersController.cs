881b390e90b25e215ebb800a7283fe5d46f04642
Shema-USK <illya.shema4527482@gmail.com>
2026-05-28 10:49:53 +0300

    Add teacher creation controller and view
    Implemented TeachersController with GET/POST actions for creating teachers. Added a styled Create.cshtml view for teacher registration. Included an unused Create.cshtml.cs PageModel stub.

---
+++ DB_ex_proj/Controllers/TeachersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DB_ex_proj.Models;
+
+namespace DB_ex_proj.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Teacher teacher)
+        {
+            ModelState.Remove("CourseAssignments");
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(teacher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(teacher);
+        }
+    }
+}
