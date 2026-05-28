d76eebd7602e2f3b9eefb730cfdc018d0a610682
Shema-USK <illya.shema4527482@gmail.com>
2026-05-28 10:45:44 +0300

    Add group creation UI and controller, restore auditorium form
    Introduce GroupsController with Create actions and SelectList for auditoriums. Add Create.cshtml and code-behind for both Auditorium and Group entities, including Bootstrap-styled forms for data entry and navigation.

---
+++ DB_ex_proj/Controllers/GroupsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DB_ex_proj.Models;
+
+namespace DB_ex_proj.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GroupsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Auditoriums = new SelectList(_context.Auditoriums, "Id", "RoomNumber");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Group group)
+        {
+            ModelState.Remove("Auditorium");
+            ModelState.Remove("Students");
+            ModelState.Remove("CourseAssignments");
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(group);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Auditoriums = new SelectList(_context.Auditoriums, "Id", "RoomNumber", group.AuditoriumId);
+            return View(group);
+        }
+    }
+}
